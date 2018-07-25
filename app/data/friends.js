33de6c9e09a4035f637ab9e5c33926b27f2cc352
Robert Cortez <Nuhuhfoo@gmail.com>
2018-07-24 21:39:37 -0700

    assignment

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+  {
+    name: "Luis Guzman",
+    photo: "https://m.media-amazon.com/images/M/MV5BNzYyNjIyOTU2NF5BMl5BanBnXkFtZTcwODk3ODcxMw@@._V1_UY317_CR1,0,214,317_AL_.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "George Lopez",
+    photo: "https://i.pinimg.com/236x/fa/c9/68/fac968d29c8df1c4413b60d5a47266c8--speak-spanish-learn-spanish.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Machete",
+    photo: "http://cdn2.holytaco.com/wp-content/uploads/2011/01/danny-trejo-scowl.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Cheech Marin",
+    photo: "https://images.amcnetworks.com/ifc.com/wp-content/uploads/2014/03/cheech-and-chong.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Tommy Chong",
+    photo: "https://www.famousbirthdays.com/headshots/tommy-chong-1.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Gabriel Iglesias",
+    photo: "http://elaccampusnews.com/wp-content/uploads/2018/05/gabriel-Iglesias.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+module.exports = friends;
